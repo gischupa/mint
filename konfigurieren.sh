1110b95568bd3ed4b5e4c9f80e60b27f88e839c7
Wolfgang Höfer <hoeferwolf@t-online.de>
2025-09-22 18:43:02 +0200

    git pull

--- konfigurieren.sh
+++ konfigurieren.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cp copy-settings.sh  /usr/local/bin/copy-settings.sh
-cp copy-settings.service  /etc/systemd/system/copy-settings.service
 
+cp copy-settings.service  /etc/systemd/system/copy-settings.service
 systemctl enable copy-settings.service
+
+cp git-pull.service  /etc/systemd/system/git-pull.service
+systemctl enable git-pull.service
+
 
 echo "tmpfs                                     /home/schueler tmpfs   defaults   0 0" >> /etc/fstab
 
