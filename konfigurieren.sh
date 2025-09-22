54ffb6e0fa69f8a8339283ad4494d1c62d4460a6
Wolfgang Höfer <hoeferwolf@t-online.de>
2025-09-22 19:21:55 +0200

    diverse Anpassungen

--- konfigurieren.sh
+++ konfigurieren.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp copy-settings.sh  /usr/local/bin/copy-settings.sh
+cp copy_settings.sh  /usr/local/bin/copy-settings.sh
 
 cp copy-settings.service  /etc/systemd/system/copy-settings.service
 systemctl enable copy-settings.service
@@ -11,7 +11,7 @@ systemctl enable git-pull.service
 
 schueler=$(grep schueler /etc/fstab)
 
-if [ "x"$schueler = "x" ];
+if [ "x$schueler" = "x" ];
   then
     echo "tmpfs                                     /home/schueler tmpfs   defaults   0 0" >> /etc/fstab
   fi
