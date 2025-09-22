f8f3aa4530a5c9245e5b97d234449204d8d3bb17
Wolfgang Höfer <hoeferwolf@t-online.de>
2025-09-22 19:32:07 +0200

    pull repariert

--- konfigurieren.sh
+++ konfigurieren.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cp copy_settings.sh  /usr/local/bin/copy-settings.sh
-
 cp copy-settings.service  /etc/systemd/system/copy-settings.service
 systemctl enable copy-settings.service
 
+
+cp git_pull.sh.sh  /usr/local/bin/git_pull.sh
 cp git-pull.service  /etc/systemd/system/git-pull.service
 systemctl enable git-pull.service
 
